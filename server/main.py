ebfe214440586709f1e97fbb5e62abd5b8227987
Debskij <debskij99@gmail.com>
2020-04-26 02:10:26 +0200

    Remove comments from mock client

--- server/main.py
+++ server/main.py
@@ -6,13 +6,10 @@ s = socket.socket()
 s.connect(('localhost', 63300))
 
 k = list(sys.argv)
-# nick = input("przedstaw sie: ")
-# gametype = input("w ile osob chcesz zagrac")
 nick, gametype = k[1].strip(), k[2].strip()
 message = nick + '♞' + gametype
 s.send(message.encode())
-data = ""
-while data != "ready":
+while True:
     data = s.recv(1024)
     data = data.decode()
     print(data)
