73149313bff29ecb21637226c73c0ca23fa54f9a
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-04-28 16:09:32 +0200

    Make mock Client bootstrap use configuration file

--- client/main.py
+++ client/main.py
@@ -3,9 +3,9 @@ from Client import Client
 
 def main():
     username = input("Username: ")
-    client = Client(username, "localhost", 63300)
-    if client.connect() == 0:
-        client.join_queue()
+    client = Client(username)
+    client.connect()
+    client.join_queue()
 
 
 if __name__ == "__main__":
