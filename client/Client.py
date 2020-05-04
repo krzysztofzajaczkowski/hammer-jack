9beedd2ad55a4f6636706b58eee91a4a0dadb072
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2020-05-04 21:52:06 +0200

    Add graceful shutdown and change server response handling

--- client/Client.py
+++ client/Client.py
@@ -1,7 +1,7 @@
 import yaml
 import socket
 import time
-
+import ast
 
 class Client(object):
     def __init__(self, username):
@@ -23,34 +23,46 @@ class Client(object):
             return -1
 
     def join_queue(self):
-        message = f"{self.username}♞JOIN_QUEUE"
+        message = f"{self.username}♞GAME_STATE"
         self.socket.send(message.encode())
         data = self.socket.recv(2048)
         data = data.decode()
-        print(f"Response from server: {data}")
-        if data == f"{self.username}♞JOINED_QUEUE":
-            print("Joined queue...")
+        try:
+            data = int(data)
+            # if data is int, then it's a place in queue
+            # print(f"Your place in queue: {data}")
             print("Press CTRL + C to leave")
             self.wait_in_queue()
-        if data == f"{self.username}♞REJECTED_QUEUE":
-            print("Couldn't join queue...") # REJECTED_QUEUE -> Server cannot establish user(sth like keyError if user
-            # with the same username already exists)
+        except ValueError:
+            # if data is not int, then we're not in queue
+            print("Couldn't join queue...")
             self.socket.shutdown(socket.SHUT_RDWR)
             self.socket.close()
 
     def wait_in_queue(self):
         try:
             while True:
-                message = f"{self.username}♞IN_QUEUE"
+                # message = f"{self.username}♞GAME_STATE"
+                message = f"GAME_STATE"
                 self.socket.send(message.encode())
                 data = self.socket.recv(2048)
                 data = data.decode()
-                print(f"Response from server: {data}")
-                if data == "DISCONNECT":
-                    print("Server closed connection")
-                    self.socket.shutdown(socket.SHUT_RDWR)
-                    self.socket.close()
-                    break
+                try:
+                    # if data is int, then it's a place in queue
+                    data = int(data)
+                    print(f"Your place in queue: {data}")
+                except ValueError:
+                    try:
+                        # if data is not int, then it's a list of users to create game
+                        data = ast.literal_eval(data)
+                        print(f"Response from server: {data}")
+                        # prepare to connect with others
+                        break
+                    except ValueError:
+                        # if not list, then server closed connection
+                        print("Server closed connection")
+                        self.socket.close()
+                        break
                 time.sleep(1.0)
         except KeyboardInterrupt:
             print("\nLeaving queue")
@@ -58,3 +70,5 @@ class Client(object):
             self.socket.send(message.encode())
             self.socket.shutdown(socket.SHUT_RDWR)
             self.socket.close()
+        self.socket.shutdown(socket.SHUT_RDWR)
+        self.socket.close()
